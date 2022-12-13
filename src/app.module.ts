ea9735ddde72b6498e6b2684c0bfe6e1d5dfab7f
josea24 <josealena24@gmail.com>
2022-12-13 16:26:06 -0400

    se agregaron todos los archivos de controladores

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
+import { UsersController } from './controllers/users.controller';
+import { BrandController } from './controllers/brand.controller';
+import { OrdersController } from './controllers/orders.controller';
+import { CustomerController } from './controllers/customer.controller';
+import { OrdersController } from './controllers/orders.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
+  controllers: [AppController, ProductsController, CategoriesController, UsersController, BrandController, OrdersController, CustomerController],
   providers: [AppService],
 })
 export class AppModule {}
